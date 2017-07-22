f65a0bf9cc196688aa325192163913b9f8ed3d9f
Nipun David <nipun18david@gmail.com>
2017-07-22 23:16:29 +0530

    Android Plugin for Unity3D - Passing Message from a Alert Dialog Box to Unity3D - Part II

---
+++ Assets/Scripts/AndroidCallBacks.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class AndroidCallBacks : MonoBehaviour {
+
+    private string yes = "0";
+    private string no = "1";
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void HandleCallBacks(string buttonIndex)
+    {
+        Debug.Log(buttonIndex);
+        if (buttonIndex == yes)
+        {
+            GameObject.FindGameObjectWithTag("controller").GetComponent<Controller>().reply.text = "Yes Pressed";
+        }else if(buttonIndex == no)
+        {
+            GameObject.FindGameObjectWithTag("controller").GetComponent<Controller>().reply.text = "No Pressed";
+        }
+    }
+}
---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Controller : MonoBehaviour {
+    // To display output of the alert box
+    public Text reply;
+ 
+    // To store context of the main activity that Unity3D is running
+    private AndroidJavaObject activityContext = null;
+
+    // To store the instance of the class
+    private AndroidJavaObject nativeDialogBox = null;
+
+    void Start()
+    {
+        AndroidCallBacks androidCallbacks = new GameObject("AndroidCallBacks").AddComponent<AndroidCallBacks>();
+
+        // Get MainActivity class instance that Unity is running
+        using (AndroidJavaClass activityClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+        {
+            // Get context of the MainActivity class that unity is running
+            activityContext = activityClass.GetStatic<AndroidJavaObject>("currentActivity");
+        }
+    }
+
+    public void ShowNativeDialogBox()
+    {
+        CallNativeDialogBox("Dialog Box", "This is a Dialog box", "Yes", "No");
+    }
+
+    private void CallNativeDialogBox(params object[] args)
+    {
+        AndroidJavaObject bridge = new AndroidJavaObject("com.plugin.android.dialogboxmodule.ShowNativeDialogBox");
+
+        activityContext.Call("runOnUiThread", new AndroidJavaRunnable(() =>
+        {
+            bridge.CallStatic("ShowDialogPopup", args);
+        }));
+    }
+}
